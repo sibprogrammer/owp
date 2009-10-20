2fd8a156a53112c2f5d62c85cbbf5a73699cce93
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-10-20 16:30:21 +0000

    add script for automated product building
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@284 5ed322ce-bccc-11de-a0e9-83d03c287164

---
+++ build/build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -xv
+
+cd `dirname $0`
+
+{
+	PROJECT="ovz-web-panel"
+	SVN_REPO="http://ovz-web-panel.googlecode.com/svn/trunk/"
+
+	[ -d $PROJECT ] && rm -rf ovz-web-panel
+
+	REVISION=`svn info $SVN_REPO | grep "Revision:" | sed "s/Revision: //g"`
+
+	if [ -d $PROJECT.r$REVISION ]; then
+		mkdir $PROJECT
+		cp -R $PROJECT.r$REVISION/* $PROJECT/
+	else
+		svn export $SVN_REPO $PROJECT
+
+		echo $REVISION > $PROJECT/revision
+
+		mkdir $PROJECT.r$REVISION
+		cp -R $PROJECT/* $PROJECT.r$REVISION/
+	fi
+
+	cd $PROJECT
+		rm -rf build tests
+
+		cd application/externals/
+			unzip -q Zend.zip
+			patch -p0 < Zend.patch
+			rm Zend.zip Zend.patch
+		cd ../../
+	cd ..
+
+	[ -f $PROJECT-r$REVISION.tgz ] && rm $PROJECT-r$REVISION.tgz
+	tar --owner 0 -czf $PROJECT-r$REVISION.tgz ./$PROJECT
+
+} 2>&1 | tee build.log
