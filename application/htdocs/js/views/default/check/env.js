2fef45a0b9d5b988a364193e9d4d1cb8b31055e6
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-10-27 16:12:13 +0000

    improvements of environment checker script
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@285 5ed322ce-bccc-11de-a0e9-83d03c287164

--- application/htdocs/js/views/default/check/env.js
+++ application/htdocs/js/views/default/check/env.js
@@ -1,25 +1,25 @@
-Owp.Views.Default.Check.Env.renderCheckEnvWindow = function(phpExtensionsJsonData) {
+Owp.Views.Default.Check.Env.renderCheckEnvWindow = function(checksData) {
 	function columnStateRenderer(state) {
 		return (state)
 			? '<img src="/skins/win_xp/images/ok.png">'
 			: '<img src="/skins/win_xp/images/off.gif">';
 	}
-	
+
 	var store = new Ext.data.SimpleStore({
 		fields: [
 			{ name: 'state' },
 			{ name: 'name' }
 		]
 	});
-	
-	store.loadData(phpExtensionsJsonData);
-	
+
+	store.loadData(checksData);
+
 	var grid = new Ext.grid.GridPanel({
 		store: store,
 		cm: new Ext.grid.ColumnModel([
 			{ id: 'state', header: "State", renderer: columnStateRenderer, width: 65,
 				align: 'center', sortable: true, dataIndex: 'state' },
-			{ id: 'name', header: "Extension name", sortable: true, dataIndex: 'name' }
+			{ id: 'name', header: "Parameter", sortable: true, dataIndex: 'name' }
 		]),
 		stripeRows: true,
 		autoExpandColumn: 'name',
@@ -33,11 +33,16 @@ Owp.Views.Default.Check.Env.renderCheckEnvWindow = function(phpExtensionsJsonDat
 			handler: function() {
 				window.location.href = '/login';
 			}
+		}, {
+		    text: 'PHP Info',
+			handler: function() {
+				window.location.href = '/check/phpinfo';
+			}
 		}]
 	});
-	
-	grid.render('phpExtensionsGrid');
-	
+
+	grid.render('checksGrid');
+
 	var checkEnvWindow = new Ext.Window({
 		applyTo: 'checkEnvWindow',
 		width: 650,
@@ -47,6 +52,6 @@ Owp.Views.Default.Check.Env.renderCheckEnvWindow = function(phpExtensionsJsonDat
 		resizable: false,
 		draggable: false
 	});
-	
+
 	checkEnvWindow.show();
 }
--- application/modules/default/controllers/CheckController.php
+++ application/modules/default/controllers/CheckController.php
@@ -2,56 +2,86 @@
 /**
  * Controller for various checks
  *
- * @author Alexei Yuzhakov <alex@softunity.com.ru> 
+ * @author Alexei Yuzhakov <alex@softunity.com.ru>
  */
-class CheckController extends Owp_Controller_Action_Simple {
-	
-	/**
-	 * Default action
-	 *
-	 */
-	public function indexAction() {
-		$this->_forward('env');
-	}
-	
-	/**
-	 * Check environment
-	 *
-	 */
-	public function envAction() {
-		$this->view->pageTitle = "Environment checker";
-		
-		$phpExtensions = array(
-			'pdo',
-			'pdo_sqlite',
-			'session',
-			'pcre',
-			'SimpleXML',
-			'SPL',
-		);
-		
-		$phpExtensionsJsonData = array();
-		
-		foreach ($phpExtensions as $phpExtension) {
-			$phpExtensionsJsonData[] = array(extension_loaded($phpExtension), $phpExtension);
-		}
-				
-		$this->view->phpExtensionsJsonData = Zend_Json::encode($phpExtensionsJsonData);
-	}
-	
-	/**
-	 * Display phpinfo information
-	 *
-	 */
-	public function phpinfoAction() {
-		if (!$this->_config->debug->enabled) {
-			$this->_redirect('/check/env');
-		}
-		
-		$this->_helper->layout->disableLayout();
-		$this->_helper->viewRenderer->setNoRender();
-		
-		phpinfo();
-	}
-			
+class CheckController extends Owp_Controller_Action_Simple
+{
+
+    /**
+     * Controller init routines
+     *
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (!$this->_config->debug->enabled) {
+            $this->_redirect('/login');
+        }
+    }
+
+    /**
+     * Default action
+     *
+     */
+    public function indexAction()
+    {
+        $this->_redirect('/check/env');
+    }
+
+    /**
+     * Check environment
+     *
+     */
+    public function envAction()
+    {
+        $this->view->pageTitle = "Environment checker";
+
+        $phpExtensions = array('pdo', 'pdo_sqlite', 'session', 'pcre', 'SimpleXML', 'SPL');
+
+        $checksData = array();
+
+        foreach ($phpExtensions as $phpExtension) {
+            $checksData[] = array(extension_loaded($phpExtension), "Extension: $phpExtension");
+        }
+
+        $phpParameters = array('safe_mode' => false, 'register_globals' => false);
+
+        foreach ($phpParameters as $phpParameter => $value) {
+            $checksData[] = array($value == ini_get($phpParameter), "PHP parameter: $phpParameter");
+        }
+
+        $checksData[] = array($this->_checkDbConnection(), "Database connection and state");
+
+        $this->view->checksData = Zend_Json::encode($checksData);
+    }
+
+    /**
+     * Display phpinfo information
+     *
+     */
+    public function phpinfoAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+
+        phpinfo();
+    }
+
+    /**
+     * Check database connection and state
+     *
+     * @return bool
+     */
+    private function _checkDbConnection()
+    {
+        try {
+            $db = Zend_Registry::get('db');
+            $db->fetchAll('SELECT * FROM users LIMIT 1');
+            return true;
+        } catch (Zend_Db_Exception $exception) {
+            return false;
+        }
+    }
+
 }
