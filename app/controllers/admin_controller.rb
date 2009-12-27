7fa6c179bb5d208b52d7a73529f2a3a03d268ff9
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2009-12-27 16:08:38 +0000

    Show physical servers in the main menu for easy access.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@415 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,18 @@
 class AdminController < ApplicationController
   layout 'admin'  
-  before_filter :login_required
+  before_filter :login_required, :servers_list
+  
+  private
+  
+  def servers_list
+    @servers_list = HardwareServer.all
+    @servers_list.map! { |server| {
+      :cls => 'menu-item',
+      :text => server.host,
+      :href => '/admin/hardware-servers/show?id=' + server.id.to_s,
+      :icon => '/images/server.png',
+      :leaf => true
+    }}
+  end
+  
 end
