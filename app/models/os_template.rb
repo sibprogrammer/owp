3c89f993983935d54133a2e429d8d35307165ee9
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-01-21 15:03:13 +0000

    Fixes issue 23 Implementing ability to install OS templates from mirrors.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@442 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/models/os_template.rb
+++ app/models/os_template.rb
@@ -4,19 +4,19 @@ class OsTemplate < ActiveRecord::Base
   belongs_to :hardware_server
   
   def self.get_available_official
-    get_available('/template/precreated/')
+    get_available(AppConfig.os_templates.mirror.path + '/precreated/')
   end
   
   def self.get_available_contributed
-    get_available('/template/precreated/contrib/')
+    get_available(AppConfig.os_templates.mirror.path + '/precreated/contrib/')
   end
   
   def self.install_official(hardware_server, name)
-    self.download(hardware_server, '/template/precreated/', name)
+    self.download(hardware_server, AppConfig.os_templates.mirror.path + '/precreated/', name)
   end
   
   def self.install_contributed(hardware_server, name)
-    self.download(hardware_server, '/template/precreated/contrib/', name)
+    self.download(hardware_server, AppConfig.os_templates.mirror.path + '/precreated/contrib/', name)
   end
 
   def delete_physically
@@ -27,14 +27,15 @@ class OsTemplate < ActiveRecord::Base
   private
   
   def self.get_available(dir)
-    ftp = Net::FTP.new('download.openvz.org')
+    ftp = Net::FTP.new(AppConfig.os_templates.mirror.host)
     ftp.login
     ftp.chdir(dir)
     ftp.nlst().find_all { |file| file =~ /tar.gz$/  }
   end
   
   def self.download(hardware_server, path, name)
-    hardware_server.rpc_client.job('wget', "-P /vz/template/cache/ ftp://download.openvz.org/#{path}/#{name}.tar.gz")
+    hardware_server.rpc_client.job('wget', "-P /vz/template/cache/ ftp://" +
+      AppConfig.os_templates.mirror.host + "/#{path}/#{name}.tar.gz")
   end
   
 end
