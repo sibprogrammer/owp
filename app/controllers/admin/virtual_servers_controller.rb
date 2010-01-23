6fbd3f060d2b44aeebe942a7bccc1374c64310d4
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-01-23 13:03:44 +0000

    Adding memory and disk space information to virtual servers list.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@449 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/controllers/admin/virtual_servers_controller.rb
+++ app/controllers/admin/virtual_servers_controller.rb
@@ -3,13 +3,15 @@ class Admin::VirtualServersController < AdminController
   def list_data
     hardware_server = HardwareServer.find_by_id(params[:hardware_server_id])
     virtual_servers = hardware_server.virtual_servers
-    virtual_servers.map! { |item| {
-      :id => item.id,
-      :identity => item.identity,
-      :ip_address => item.ip_address,
-      :host_name => item.host_name,
-      :state => item.state,
-      :os_template_name => item.os_template ? item.os_template.name : '-'
+    virtual_servers.map! { |virtual_server| {
+      :id => virtual_server.id,
+      :identity => virtual_server.identity,
+      :ip_address => virtual_server.ip_address,
+      :host_name => virtual_server.host_name,
+      :state => virtual_server.state,
+      :os_template_name => virtual_server.os_template ? virtual_server.os_template.name : '-',
+      :diskspace => virtual_server.diskspace,
+      :memory => virtual_server.memory,
     }}
     render :json => { :data => virtual_servers }  
   end
