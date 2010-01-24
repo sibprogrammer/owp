540238404dcd1923e230774210530b7e73192f73
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-01-24 12:08:42 +0000

    Fixes issue 20 Implementing CRUD for panel users.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@450 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,4 +17,51 @@ class Admin::UsersController < AdminController
     end
   end
   
+  def list
+    @up_level = '/admin/dashboard'
+  end
+  
+  def list_data
+    users = User.all
+    users.map! { |user| {
+      :id => user.id,
+      :login => user.login,
+      :created_at => user.created_at.strftime("%Y.%m.%d %H:%M:%S"),
+    }}
+    render :json => { :data => users }  
+  end
+  
+  def delete
+    params[:ids].split(',').each { |id|
+      user = User.find(id) 
+      
+      if !user.delete
+        render :json => { :success => false }  
+        return
+      end
+    }
+    
+    render :json => { :success => true }
+  end
+  
+  def update
+    user = (params[:id].to_i > 0) ? User.find_by_id(params[:id]) : User.new
+    user.attributes = params
+    
+    if user.save
+      render :json => { :success => true }  
+    else
+      render :json => { :success => false, :form_errors => user.errors }
+    end
+  end
+  
+  def load_data
+    user = User.find_by_id(params[:id])
+    redirect_to :controller => 'users', :action => 'list' and return if !user
+    
+    render :json => { :success => true, :data => {
+      :login => user.login,
+    }}
+  end
+  
 end
