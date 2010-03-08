d90e284e7da2309e0c81ad3cf52e0348b654a6af
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-03-08 07:19:57 +0000

    Returning back session routes.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@501 5ed322ce-bccc-11de-a0e9-83d03c287164

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
 
+  map.resource :session
+
   map.namespace :admin do |admin|
     admin.connect '/hardware-servers/:action', :controller => 'hardware_servers'
     admin.connect '/virtual-servers/:action', :controller => 'virtual_servers'
