a74c912f7442aea91f9e2fed806ce2717e4d227b
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-05-13 00:52:37 +0000

    Fixes issue 110 Skip validation of virtual server's properties during synchronization process.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@602 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/models/hardware_server.rb
+++ app/models/hardware_server.rb
@@ -103,7 +103,7 @@ class HardwareServer < ActiveRecord::Base
       virtual_server.diskspace = parser.get('DISKSPACE').split(":").last.to_i / 1024
       virtual_server.memory = parser.get('PRIVVMPAGES').split(":").last.to_i * 4 / 1024
       virtual_server.hardware_server = self
-      virtual_server.save
+      virtual_server.save(false)
     }
   end
   
