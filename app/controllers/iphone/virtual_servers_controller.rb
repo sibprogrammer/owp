a3259a859ac3a1e9271868d38eb7f4f4ba74f9db
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-06-10 15:20:08 +0000

    Fixes issue 69 * virtual server details screen * virtual servers list for end user
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@628 5ed322ce-bccc-11de-a0e9-83d03c287164

---
+++ app/controllers/iphone/virtual_servers_controller.rb
@@ -0,0 +1,23 @@
+class Iphone::VirtualServersController < Iphone::Base
+
+  def list
+    @page_title = t('admin.virtual_servers.title')
+    
+    @virtual_servers = @current_user.virtual_servers
+    @virtual_servers.map! { |virtual_server| {
+      :id => virtual_server.id,
+      :identity => virtual_server.identity,
+      :ip_address => virtual_server.ip_address.blank? ? '' : virtual_server.ip_address.split.join(', '),
+      :host_name => virtual_server.host_name,
+      :description => virtual_server.description,
+    }}
+  end
+  
+  def show
+    @virtual_server = VirtualServer.find_by_id(params[:id])
+    redirect_to :controller => 'dashboard' and return if !@virtual_server or !@current_user.can_control(@virtual_server)
+    
+    @page_title = "##{@virtual_server.identity}"
+  end
+  
+end
