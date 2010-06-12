94fb79ecf101950ad2b9b0780378b4ccca6ac25f
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-06-12 16:07:14 +0000

    Fixes issue 117 Add validation for description if storing to config is used.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@633 5ed322ce-bccc-11de-a0e9-83d03c287164

--- app/models/virtual_server.rb
+++ app/models/virtual_server.rb
@@ -14,6 +14,7 @@ class VirtualServer < ActiveRecord::Base
   validates_format_of :nameserver, :with => /^((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|\s)*$/
   validates_format_of :search_domain, :with => /^([a-z0-9\-\.]+\s*)*$/i
   validates_format_of :host_name, :with => /^[a-z0-9\-\.]*$/i
+  validates_format_of :description, :with => /^[a-z0-9\-\.\s]*$/i if AppConfig.vzctl.save_descriptions
 
   def get_limits
     parser = IniParser.new(hardware_server.rpc_client.exec('cat', "/etc/vz/conf/#{identity.to_s}.conf")['output'])
