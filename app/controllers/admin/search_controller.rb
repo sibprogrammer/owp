82d62e4cadcc67083a9b32cec8b80df681e0f8d5
sibprogrammer <sibprogrammer@5ed322ce-bccc-11de-a0e9-83d03c287164>
2010-07-26 15:21:36 +0000

    Fixes issue 109 Quick search for virtual servers.
    git-svn-id: http://ovz-web-panel.googlecode.com/svn/trunk@658 5ed322ce-bccc-11de-a0e9-83d03c287164

---
+++ app/controllers/admin/search_controller.rb
@@ -0,0 +1,31 @@
+class Admin::SearchController < Admin::Base
+  before_filter :superadmin_required
+  
+  def index
+    search_fields = [ "identity", "description", "host_name", "ip_address", "orig_os_template", "orig_server_template" ]
+    virtual_servers = VirtualServer.find(:all, :conditions => [
+      search_fields.map{ |item| item + " LIKE :query" }.join(' OR '), { :query => "%" + params[:query] + "%" }
+    ])
+    
+    result = []
+    
+    virtual_servers.each { |virtual_server|
+      description =
+        "<a href='/admin/virtual-servers/show?id=#{virtual_server.id}'>" +
+        t("admin.virtual_servers.show.title", :name => "#" + virtual_server.identity.to_s) +
+        "</a><br/>" +
+        (virtual_server.description.blank? ? '' : virtual_server.description + "<br>") +
+        [ 
+          virtual_server.host_name,
+          virtual_server.ip_address,
+          virtual_server.orig_os_template,
+          virtual_server.orig_server_template,
+          virtual_server.hardware_server.host
+        ].reject(&:blank?).join(', ')
+      result << { :item => description }
+    }
+    
+    render :json => { :data => result }
+  end
+  
+end
