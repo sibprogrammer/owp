7b038a856e7912ef3aab0e4d5103781ba23ae3a1
Miguel Pontes <miguelpontes@gmail.com>
2013-04-21 02:58:51 +0100

    Needs limit => nil to undo the 255 from the varchar.

--- db/migrate/20130421012626_undo_change_identity_type_in_virtual_server.rb
+++ db/migrate/20130421012626_undo_change_identity_type_in_virtual_server.rb
@@ -1,6 +1,6 @@
 class UndoChangeIdentityTypeInVirtualServer < ActiveRecord::Migration
   def self.up
-    change_column :virtual_servers, :identity, :integer
+    change_column :virtual_servers, :identity, :integer, :limit => nil
   end
 
   def self.down
