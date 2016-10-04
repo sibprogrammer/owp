c5ff6cba8ddfd4345269db1da7ed7559bd50d09c
amidevous <amidevous@users.noreply.github.com>
2016-10-04 23:51:01 +0200

    Update ai.sh

--- installer/ai.sh
+++ installer/ai.sh
@@ -2,7 +2,7 @@
 
 # global variables
 VERSION="2.4"
-DOWNLOAD_URL="http://ovz-web-panel.googlecode.com/files/ovz-web-panel-$VERSION.tgz"
+DOWNLOAD_URL="https://github.com/amidevous/owp/archive/master.tar.gz"
 RUBYGEMS_URL="http://production.cf.rubygems.org/rubygems/rubygems-1.3.5.tgz"
 RUBY_SQLITE3_CMD="ruby -e \"require 'rubygems'\" -e \"require 'sqlite3'\""
 LOG_FILE="/tmp/ovz-web-panel.log"
